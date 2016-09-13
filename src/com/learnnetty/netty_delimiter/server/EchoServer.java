42d86acea554c51d48dd08b1a8aebf82cbb2b25e
evsward <iliuwb@163.com>
2016-09-13 14:00:19 +0800

    s

--- src/com/learnnetty/netty_delimiter/server/EchoServer.java
+++ src/com/learnnetty/netty_delimiter/server/EchoServer.java
@@ -36,7 +36,7 @@ public class EchoServer {
 						@Override
 						protected void initChannel(SocketChannel ch) throws Exception {
 							ByteBuf delimiter = Unpooled.copiedBuffer("$_".getBytes());
-//							ch.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, delimiter));
+							ch.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, delimiter));
 							ch.pipeline().addLast(new StringDecoder());
 							ch.pipeline().addLast(new EchoServerHandler());
 						}
